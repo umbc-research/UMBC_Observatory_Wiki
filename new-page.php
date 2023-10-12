8d2dd0f039fc5b52b356664c6c40c790b988d866
GabrielKoletsi <141643049+GabrielKoletsi@users.noreply.github.com>
2023-10-12 12:56:19 -0400

    Update new-page.php

--- new-page.php
+++ new-page.php
@@ -1,5 +1,5 @@
 <div class="nav navbar-nav" id="dw__addnewpage">
-    <a href="javascript:void(0);" id="createBlog" class="btn btn-primary" role="button" title="<?php echo tpl_getLang('add_new_page'); ?>">
+    <a href="javascript:void(0);" id="createBlog" class="btn btn-icon-centered" role="button" title="<?php echo tpl_getLang('add_new_page'); ?>">
         <?php echo iconify('mdi:file-plus'); ?>
     </a>
 </div>
@@ -19,3 +19,15 @@
 
 document.getElementById('createBlog').addEventListener('click', createBlogOnClick);
 </script>
+
+<style>
+	.btn-icon-centered {
+		color: white;
+		margin-top: 7px;
+	}
+
+	.btn-icon-centered:hover {
+		transition: 1s;
+		color: #00f572;
+	}
+</style>
