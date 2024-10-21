2b51d99f9cd360ddca6a5dd39b65019bc4653836
navyassassin <mb17@umbc.edu>
2024-10-21 13:53:28 -0400

    changed to more accurately reflect new vm

--- blog_monitor.sh
+++ blog_monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PATH="/var/www/obs-web.rs.umbc.edu/data/pages/blog"
+PATH="/var/www/html/data/pages/blog/"
 /bin/inotifywait -m -r -e create,delete "$PATH" |
 	while read -r directory event file
 	do
 		echo "Change in $file was $event"
-		./bin/blog "$file" "$event"
+		/bin/blog "$file" "$event"
 	done
